f8cf05624c2bb36a61437e0f1361f85235acd156
root <root@jenkins-server.asia-south1-a.c.vibrant-petal-317106.internal>
2021-08-08 09:23:42 +0000

    My Proj

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJs',
+	    'sonar.login': '',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
